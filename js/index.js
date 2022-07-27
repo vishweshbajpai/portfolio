ef44700f70bc9da731926d52a437332def38ce1b
vishweshbajpai <vishweshbajpai16@gmail.com>
2022-07-27 18:26:05 +0530

    fixed mobile css

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 window.onscroll = () => {
-    const scrollButton = document.getElementById("scrollButton");
-    if(document.documentElement.scrollTop > 200) {
-        scrollButton.style.display = "block";
-    }
-    else {
-        scrollButton.style.display = "none";
-    }
-}
+  const scrollButton = document.getElementById("scrollButton");
+  if (document.documentElement.scrollTop > 200) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+};
 
 const scrollClickHandler = () => {
-    document.documentElement.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
-}
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};

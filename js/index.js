cd8544b6dd4e818846185efdc3b3cc4bdf11199f
vishweshbajpai <vishweshbajpai16@gmail.com>
2022-07-28 16:45:16 +0530

    added new section

--- js/index.js
+++ js/index.js
@@ -13,3 +13,28 @@ const scrollClickHandler = () => {
     behavior: "smooth",
   });
 };
+
+const footerFormSubmitHandler = () => {
+  window.event.preventDefault();
+  const form = document.getElementById("footerForm");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const description = document.getElementById("description");
+  if (!form.checkValidity()) {
+    form.classList.add("was-validated");
+  } else {
+    const toastLiveExample = document.getElementById("liveToast");
+    const toast = new bootstrap.Toast(toastLiveExample);
+    toast.show();
+    name.value = "";
+    email.value = "";
+    phone.value = "";
+    description.value = "";
+    form.classList.remove("was-validated");
+  }
+};
+
+const SendClickHandler = () => {
+  
+};
